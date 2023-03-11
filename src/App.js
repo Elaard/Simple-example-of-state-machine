b407e48545f0cec7846539af2ece603a4ae02f3f
Elaard <bartek.dv.89@gmail.com>
2023-03-11 11:24:25 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import StateMachineExample from "./StateMachineExample";
+
+function App() {
+  return (
+    <div className="App">
+      <StateMachineExample />
+    </div>
+  );
+}
+
+export default App;
